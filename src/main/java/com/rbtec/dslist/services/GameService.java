fd7b45e8f7ad2ea443064cb7769b746053ceef85
Ronaldo Barros <ronaldobarros1008@gmail.com>
2023-07-17 23:21:29 -0300

    Deploy ready

--- src/main/java/com/rbtec/dslist/services/GameService.java
+++ src/main/java/com/rbtec/dslist/services/GameService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.rbtec.dslist.dto.GameDTO;
 import com.rbtec.dslist.dto.GameMinDTO;
 import com.rbtec.dslist.entities.Game;
+import com.rbtec.dslist.projections.GameMinProjection;
 import com.rbtec.dslist.repositories.GameRepository;
 
 @Service
@@ -33,6 +34,13 @@ public class GameService {
 		//List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
 		//return dto;
 		//ou
+		return result.stream().map(x -> new GameMinDTO(x)).toList();
+		
+	}
+	
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findByList(Long listId){
+		List<GameMinProjection> result = gameRepository.searchByList(listId);
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
 		
 	}
